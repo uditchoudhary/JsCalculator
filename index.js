d2f0c412cbd689d4327a9eee55e18a5b641b4c64
Udit Choudhary <uditchoudhary@gmail.com>
2022-02-28 12:08:49 -0500

    full functional. need some css

---
+++ index.js
@@ -0,0 +1,30 @@
+let display = document.getElementById("calculation");
+let buttons = Array.from(document.getElementsByClassName("button"));
+buttons.map(button => {
+    button.addEventListener('click', e => {
+        let buttonClicked = e.target.textContent;
+        console.log(buttonClicked);
+        switch (buttonClicked) {
+        case 'Del':
+            if (display.textContent)
+              display.innerText = display.innerText.slice(0, -1); 
+                break;
+        case '=':
+            try{
+            display.innerText = eval(display.innerText)
+            } catch(Exception){
+                display.innerText='errrrrrr'
+            }
+            break;
+        case 'C':
+            display.textContent = "";
+            break;
+        default:
+            if (display.innerText === "errrrrrr"){
+                display.innerText = '';
+            }
+            display.innerText += buttonClicked;
+        }
+        
+    })
+})
